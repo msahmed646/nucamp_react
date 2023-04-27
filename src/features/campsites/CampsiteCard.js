e3fef66a1f9b3441ee9e8a6f6e38c6e809ca6af6
msahmed646 <msahmed646@gmail.com>
2023-04-27 09:59:18 -0400

    React week_2 material start

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,12 +1,13 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
     return (
         <Card>
             <CardImg 
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
+                src={image}
+                alt={name}
             />
 
             <CardImgOverlay>
