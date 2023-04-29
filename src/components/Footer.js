e4bfdc2eaabee57e4cb8b36ec41cf095783d133c
msahmed646 <msahmed646@gmail.com>
2023-04-29 00:52:43 -0400

    Add header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return(
-        <footer className="site-footer">
+    return (
+        <footer className='site-footer'>
             <Container>
                 <Row>
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                        <h5>this will be site navigation!</h5>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
                     </Col>
-
                     <Col xs='6' sm='3' className='text-center'>
                         <h5>Social</h5>
                         <a
@@ -36,7 +50,6 @@ const Footer = () => {
                             <i className='fa fa-youtube' />
                         </a>
                     </Col>
-
                     <Col sm='4' className='text-center'>
                         <a
                             role='button'
@@ -58,5 +71,6 @@ const Footer = () => {
             </Container>
         </footer>
     );
-}
+};
+
 export default Footer;
