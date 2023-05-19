8dcc31887aa210e090f8454fcf901cca7cad530e
msahmed646 <msahmed646@gmail.com>
2023-05-19 15:32:25 -0400

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,8 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
-}
+};
 
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
@@ -10,4 +22,4 @@ export const selectCampsiteById = (id) => {
 
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
-}
+};
