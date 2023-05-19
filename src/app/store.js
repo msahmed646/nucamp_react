5d59b179fc743887512637e3fe83bb8c8de8c147
msahmed646 <msahmed646@gmail.com>
2023-05-19 18:48:54 -0400

    Add MiddleWare

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +12,6 @@ export const store = configureStore({
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
-    }
+    },
+    middleware: (curryGetDefaultMiddleware) => curryGetDefaultMiddleware().concat([logger])
 });
